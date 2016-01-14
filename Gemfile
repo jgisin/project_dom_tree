1f3ca817a91a4cf02130cc3ba45164f34171f015
Jeffrey Gisin <jgisin@gmail.com>
2016-01-14 15:42:48 -0700

    Working on html file reader

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-# gem "rails"
+gem 'rspec'
+gem 'guard-rspec', require: false
--- lib/dom_parser.rb
+++ lib/dom_parser.rb
@@ -1,3 +1,5 @@
+#require 'pry-byebug'
+
 class DOMParser
 
   def initialize
@@ -9,7 +11,7 @@ class DOMParser
     tag_type = string.match(/<(\w+) /)[1]
     attr_hash["tag_type"] = tag_type
 
-    attribute_match_data = string.scan(/(\w+)\s?=\s?'([\w\s]+)'/)
+    attribute_match_data = string.scan(/(\w+)\s?=\s?'([\S]*[\w\s]*)'/)
     attribute_match_data.each do |match|
       key = match[0]
       values = match[1].split(" ")
@@ -20,17 +22,29 @@ class DOMParser
   end
 
   def tag_printer(tag_hash)
-    print "<#{tag_hash['tag_type']} "
+    html_string = ""
+    html_string += "<#{tag_hash['tag_type']} "
 
     tag_hash.each do |key,value|
-
+      unless key == "tag_type"
+        html_string += "#{key}='"
+        class_string = ""
+        value.each do |sub_value|
+          class_string += "#{sub_value} "
+        end
+        html_string += class_string.strip
+        html_string += "' "
+      end
     end
-
+    html_string.strip!
+    html_string += ">"
+    html_string
   end
 
 end
 
-#
-# p tag_parser("<p class='foo bar' id='baz' name='fozzie'>")
+dom = DOMParser.new
+ print dom.tag_parser("<img src='http://www.example.com' title='funny things'>")
+ dom.tag_printer(dom.tag_parser("<p class='foo bar' id='baz' name='fozzie'>"))
 # p tag_parser("<div id = 'bim'>")
 # p tag_parser("<img src='http://www.example.com' title='funny things'>")
