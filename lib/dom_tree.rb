e98b2c016e31589587bc030b5daa1408e414a46d
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-14 19:02:26 -0500

    dom parser

---
+++ lib/dom_tree.rb
@@ -0,0 +1,13 @@
+#children in an array
+Node = Struct.new(:data, :children, :parent)
+
+class DOMTree
+
+
+
+end
+
+
+# opening tag - create new node
+# content? - new node w/ content type=text or content
+# closing tag - up one level (don't add anything)
